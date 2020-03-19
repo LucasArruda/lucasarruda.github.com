56dd3b886f58bfc94d034bf89a88ef95b1c7eda7
lucasarruda <lucasarruda@gmail.com>
2020-03-19 18:51:12 -0300

    New website

---
+++ src/data.js
@@ -0,0 +1,175 @@
+module.exports = {
+  profile: {
+    name: 'Lucas Arruda',
+    title: 'Senior Software Engineer',
+    photo: 'https://avatars2.githubusercontent.com/u/88818?s=300&v=4', // 300x300px
+    city: 'Remote, Brazil',
+    phone: '+55 (31) 98877-6707',
+    mail: 'lucasarruda@gmail.com'
+  },
+  social: {
+    twitter: {
+      name: '@lucasarruda',
+      link: 'https://twitter.com/lucasarruda'
+    },
+    linkedin: {
+      name: 'in/arrudalucas',
+      link: 'https://www.linkedin.com/in/arrudalucas'
+    },
+    github: {
+      name: 'lucasarruda',
+      link: 'https://github.com/lucasarruda'
+    }
+  },
+  pitch: `I have been working with software development for over 10 years and I am good at solving hard problems.<br/>
+  I have always been a curious person, one that is not satisfied until I discover how things work. I do that by breaking things (problems) into smaller things (parts of the problem) until I get a good picture of the whole situation. And then I tackle problems and I strive to solve them in an efficient and user-friendly way. I try to build simple, clean and effective solutions, rather than making complex and fragile solutions.<br/>
+  I also have a business mindset and I focus on making your users happy and you profit more. User satisfaction is important and so it is that a business is profitable and healthy. And those are aspects that are my focus.
+  `,
+  summary: `A full-stack Senior Software Engineer with more than 10 years of experience.<br/>I have been work with Ruby on Rails for the last 5 years and been programming in general for more than 10 (mostly using Java, PHP and Javascript). My focus is to write software that is usable, tested and testable, high quality and that is useful.<br/>I have worked with different industries: e-commerce, educational, telemetry & geospatial processing, event management and of diverse sizes, from startups to Macys and Bloomingdales.<br/>I have a passion for what I do and take pride in it. I work hard to leave everything better than when I found it.`,
+  experiences: [
+    {
+      company: 'Make School',
+      location: 'Remote',
+      title: 'Senior Software Engineer',
+      period: 'JAN 2017 - MAR 2020',
+      jobdesc: [
+        'Development of an internal platform that supports students, staff and program applicants',
+        'MakeSchool\'s Website',
+        'Building and maitaining multiple internal tools',
+        'Planning, building and handling architecure, distribution and servers',
+        'Using Ruby on Rails + GraphQL API on the Backend, React, Relay (& Apollo), GraphQL Frontend.'
+      ],
+    },
+    {
+      company: 'Pathable',
+      location: 'Remote',
+      title: 'Senior Consultant Engineer',
+      period: 'DEC 2016 - JAN 2017',
+      jobdesc: [
+        'Several bug fixes and adding features to a legacy event platform in Ruby on Rails',
+      ],
+    },
+    {
+      company: 'Lets.events',
+      location: 'Remote',
+      title: 'Senior Software Engineer',
+    period: 'JUN 2016 - NOV 2016',
+      jobdesc: [
+        'Full-stack development with Ruby on Rails, GraphQL, CoffeeScript, Slim',
+        'Creating a Event Management System for Events and Venues from scratch',
+        'Integrating with Facebook, payment gateways'
+      ],
+    },
+    {
+      company: 'Raise Sistemas',
+      location: 'Remote',
+      title: 'Senior Software Engineer',
+      period: 'JUN 2015 - JUN 2016',
+      jobdesc: [
+        'Full-stack Ruby on Rails development of a online ticket platform and a CRM for NPOs',
+        'Integrating with payment gateways and generating reports and graphs',
+        'Using Ruby on Rails, React, Flux, jQuery, HTML, CSS, '
+      ],
+    },
+    {
+      company: 'ThoughtWorks',
+      location: 'Porto Alegre, Brazil',
+      title: 'Senior Consultant Develiper',
+      period: 'NOV 2014 - JUN 2015',
+      jobdesc: [
+        'Working on internal project using Node.JS (full stack)',
+        'Building, adding features and bug fixes to a Kanban Board used internally'
+      ],
+    },
+    {
+      company: 'Avenue Code',
+      location: 'Belo Horizonte, Brazil',
+      title: 'Software Developer',
+      period: 'AUG 2011 - NOV 2014',
+      jobdesc: [
+        'Developing using Java on the Backend',
+        'Frontend work using HTML, CSS and building Javascript components.',
+        'Developing internal time tracking tool in Ruby on Rails.',
+        'Worked for Macys and Bloomingdales',
+        'Responsible for the desyndication of CHANEL website on Bloomingdales'
+      ],
+    },
+    {
+      company: 'Avenue Code',
+      location: 'Belo Horizonte, Brazil',
+      title: 'Software Developer',
+      period: 'AUG 2011 - NOV 2014',
+      jobdesc: [
+        'Developing using Java on the Backend',
+        'Frontend work using HTML, CSS and building Javascript components.',
+        'Developing internal time tracking tool in Ruby on Rails.',
+        'Worked for Macys and Bloomingdales',
+        'Responsible for the desyndication of CHANEL website on Bloomingdales'
+      ],
+    },
+  ],
+  educations: [
+    {
+      name: 'Federal University of Minas Gerais (UFMG)',
+      city: 'Belo Horizonte',
+      degree: 'Specialist - Post Grad',
+      period: '2010 - 2011',
+      faculty: 'Spec. in Software Engineering'
+    },
+    {
+      name: 'Pontifical Catholic University of Minas Gerais',
+      city: 'Belo Horizonte',
+      degree: 'Bachelor Degree',
+      period: '2004 - 2008',
+      faculty: 'Bachelor of Computer Science'
+    }
+  ],
+  projects: [
+    {
+      title: 'PokerLAB - Poker Academy',
+      company: 'Grupo VFX',
+      period: 'OCT 2016 - NOV 2016'
+    },
+    {
+      title: 'Datagoal - DataMining & Polls',
+      company: 'Datagoal',
+      period: 'JUN 2016 - NOV 2016'
+    },
+    {
+      title: 'Tablero - KANBAN board',
+      company: 'ThoughtWorks',
+      period: 'NOV 2014 - JAN 2015'
+    },
+  ],
+  skills: [
+    {
+      name: 'Ruby on Rails',
+      progress: 90
+    },
+    {
+      name: 'Java',
+      progress: 80
+    },
+    {
+      name: 'Javascript',
+      progress: 70
+    },
+    {
+      name: 'CSS',
+      progress: 80
+    },
+    {
+      name: 'HTML',
+      progress: 80
+    },
+    {
+      name: 'SQL',
+      progress: 80
+    }
+  ],
+  languages: [
+    'Portuguse',
+    'English',
+    'Spanish'
+  ]
+}
