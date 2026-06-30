e6e35938a73bd94b8dc254cae292b4af3ab646ad
Lucas Arruda <lucasarruda@gmail.com>
2026-06-30 15:40:57 -0300

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,71 +1,45 @@
-const webpack = require('webpack')
 const path = require('path')
-const glob = require('glob')
+const { globSync } = require('glob')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require("mini-css-extract-plugin")
-const CompressionPlugin = require("compression-webpack-plugin")
-const TerserJSPlugin = require('terser-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const PurgecssPlugin = require('purgecss-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CompressionPlugin = require('compression-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const { PurgeCSSPlugin } = require('purgecss-webpack-plugin')
 
 const content = require('./src/data')
 
-const SRC = path.resolve(__dirname, 'src');
-
-const PATHS = {
-  src: SRC
-}
-
-const NODE_ENV = process.env.NODE_ENV;
-
-const isDev = () => {
-  return (NODE_ENV === 'development');
-};
-
-const setPublicPath = () => {
-  return isDev() ? '/' : '';
-};
-
-const setPath = function(folderName) {
-  return path.join(__dirname, folderName);
-};
+const SRC = path.resolve(__dirname, 'src')
+const NODE_ENV = process.env.NODE_ENV || 'production'
+const isDev = () => NODE_ENV === 'development'
 
 const htmlPlugin = (inputTemplatePath, outputFileName, chunkPattern) => {
   return new HtmlWebpackPlugin({
     filename: outputFileName,
     inject: true,
-    template: setPath(inputTemplatePath),
-    chunks: [
-      chunkPattern
-    ],
-    templateParameters: {
-      content
-    },
-    minify: {
+    template: path.join(__dirname, inputTemplatePath),
+    chunks: [chunkPattern],
+    templateParameters: { content },
+    minify: isDev() ? false : {
       removeAttributeQuotes: true,
       collapseWhitespace: true,
       html5: true,
       minifyCSS: true,
       removeComments: true,
       removeEmptyAttributes: true
-    },
-    environment: process.env.NODE_ENV
-  });
+    }
+  })
 }
 
 const plugins = [
-  new webpack.DefinePlugin({
-    'process.env': {
-      NODE_ENV: '"'+NODE_ENV+'"'
-    }
-  }),
-  htmlPlugin('/src/index.ejs', 'index.html', 'dracula'),
-  htmlPlugin('/src/light.ejs', 'light.html', 'light'),
+  htmlPlugin('src/index.ejs', 'index.html', 'dracula'),
+  htmlPlugin('src/light.ejs', 'light.html', 'light'),
   new MiniCssExtractPlugin({
-    filename: "[name].[hash].css"
+    filename: '[name].[contenthash].css'
   }),
-  new PurgecssPlugin({
-    paths: glob.sync(`${PATHS.src}/*`)
+  new PurgeCSSPlugin({
+    paths: globSync(`${SRC}/**/*`, { nodir: true }),
+    safelist: ['html', 'body']
   }),
   new CompressionPlugin({
     algorithm: 'gzip'
@@ -78,16 +52,17 @@ module.exports = {
     light: './src/light-entry.js'
   },
   output: {
-    path: isDev() ? path.resolve(__dirname) : setPath('dist'),
-    publicPath: setPublicPath(),
-    filename: isDev() ? '[name].js' : '[name].[hash].js'
+    path: isDev() ? path.resolve(__dirname) : path.resolve(__dirname, 'dist'),
+    publicPath: isDev() ? '/' : '',
+    filename: isDev() ? '[name].js' : '[name].[contenthash].js',
+    clean: !isDev()
   },
   mode: isDev() ? 'development' : 'production',
-  optimization:{
-    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  optimization: {
+    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
     runtimeChunk: false,
     splitChunks: {
-      chunks: "all",
+      chunks: 'all'
     }
   },
   resolve: {
@@ -98,7 +73,7 @@ module.exports = {
   },
   devServer: {
     historyApiFallback: true,
-    noInfo: false
+    hot: true
   },
   module: {
     rules: [
@@ -107,12 +82,12 @@ module.exports = {
         use: [
           MiniCssExtractPlugin.loader,
           'css-loader',
-          'sass-loader',
-        ],
+          'sass-loader'
+        ]
       },
       {
         test: /\.m?js$/,
-        exclude: /(node_modules)/,
+        exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
@@ -122,5 +97,5 @@ module.exports = {
       }
     ]
   },
-  plugins,
+  plugins
 }
